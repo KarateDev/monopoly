f2b3dd97362b7015e57254cdfcc4e2c8484857d4
KarateDev <clement.sorin@orange.fr>
2016-05-23 16:54:10 +0200

    Modification de la collection proprietes

--- src/Jeu/Monopoly.java
+++ src/Jeu/Monopoly.java
@@ -14,17 +14,17 @@ public class Monopoly {
         des = new ArrayList<>(2);
     }
 
-	public void lancerDes() {
-		this.des.clear();
-		Random rand = new Random();
-		int de = rand.nextInt(6)+1;
-		des.add(de);
-		de = rand.nextInt(6)+1;
-		des.add(de);
-	}
+    public void lancerDes() {
+	this.des.clear();
+	Random rand = new Random();
+	int de = rand.nextInt(6)+1;
+	des.add(de);
+	de = rand.nextInt(6)+1;
+	des.add(de);
+    }
         
     public void addJoueur(Joueur j){
-		this.joueurs.add(j);
+	this.joueurs.add(j);
 	}
         
     public void addCarreau(Carreau c){
@@ -32,33 +32,33 @@ public class Monopoly {
     }
 
 		
-	public void envoyerEnPrison(Joueur j) {
-		j.setPositionCourante(this.getCarreaux().get(11));//la prison se trouve être la case 11
-	}
-        /**
-         * @return the Carreaux
-         */
-        public HashMap<Integer,Carreau> getCarreaux() {
-            return Carreaux;
-        }
-        
-        public ArrayList<Integer> getDes() {
-            return des;
-        }
+    public void envoyerEnPrison(Joueur j) {
+	j.setPositionCourante(this.getCarreaux().get(11));//la prison se trouve être la case 11
+    }
+    /**
+     * @return the Carreaux
+     */
+    public HashMap<Integer,Carreau> getCarreaux() {
+	return Carreaux;
+    }
 
-        /**
-         * @return the joueurs
-         */
-        public ArrayList<Joueur> getJoueurs() {
-            return joueurs;
-        }
-        
-        public int getSommeDes() {
-            return des.get(0)+des.get(1);
-        }
-        
-        public void eliminerJoueur(Joueur j){
-            joueurs.remove(j);
-        }
+    public ArrayList<Integer> getDes() {
+	return des;
+    }
+
+    /**
+     * @return the joueurs
+     */
+    public ArrayList<Joueur> getJoueurs() {
+	return joueurs;
+    }
+
+    public int getSommeDes() {
+	return des.get(0)+des.get(1);
+    }
+
+    public void eliminerJoueur(Joueur j){
+	joueurs.remove(j);
+    }
 
 }
--- src/Jeu/ParcPublic.java
+++ src/Jeu/ParcPublic.java
@@ -2,6 +2,8 @@ package Jeu;
 
 public class ParcPublic extends Carreau{
 
+    private int caisse ;
+    
     public ParcPublic(int num, String nomcarreau) {
         super(num, nomcarreau);
     }
