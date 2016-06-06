61ac28719bfcb787216537ceafd35404f8cfe8a4
KarateDev <clement.sorin@orange.fr>
2016-06-06 11:05:37 +0200

    Ajout des classes IHMFrame , Message et Observateur

---
+++ src/UI/IHMFrame.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UI;
+
+import javax.swing.*;
+
+/**
+ *
+ * @author sorindoc
+ */
+public class IHMFrame extends JFrame implements Observateur{
+
+    @Override
+    public void notifier(Message message) {
+	throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
